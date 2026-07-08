a5c5b5c5f73dd3550d9dc6d0194e6d7200357eaf
Emam Hasan <hemam7917@gmail.com>
2026-07-08 18:44:52 +0600

    Professional Product Card Base Structure Completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 console.log("Lesson 2 Loaded Successfully");
 
 alert("Welcome to Three Brothers Shop Pro");
+
+
+const product = {
+
+    id:1,
+
+    title:"Premium Rice",
+
+    price:1200,
+
+    image:"rice.jpg"
+
+};
