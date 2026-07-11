9340a45a341ede6c57cadbd6ad8b1cfba9994f64
Emam Hasan <hemam7917@gmail.com>
2026-07-11 17:17:09 +0600

    Module 4.5 - Lesson 3 Back To Top Button Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,31 @@ menuToggle.addEventListener("click", () => {
 
 });
 
+const backToTop = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
